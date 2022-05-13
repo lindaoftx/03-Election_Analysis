d00a00a0233b913a588572cbbd6f43fe51176ffd
lindaoftx <lindaoftexas@gmail.com>
2022-05-12 23:52:47 -0500

    Module 3 Challenge
    Completed Module 3 Challenge

--- PyPoll.py
+++ PyPoll.py
@@ -5,32 +5,70 @@ file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("Analysis","election_analysis.txt")
 
 total_votes = 0
+total_cvotes = 0
+
 candidate_options = []
 candidate_votes = {}
+
+county_options = []
+county_votes = {}
+
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+
+turnout_highest = ""
+turnout_count = 0
+turnout_percentage = 0
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
     for row in file_reader:
         total_votes += 1
+        total_cvotes += 1
         candidate_name = row[2]
+        county_name = row[1]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
+        if county_name not in county_options:
+            county_options.append(county_name)
+            county_votes[county_name] = 0
+        county_votes[county_name] += 1
 with open(file_to_save,"w") as txt_file:
     election_results = (
-        f"\n==================\n"
-        f"ELECTION RESULTS\n"
-        f"==================\n\n"
+        f"\n=========================================\n\n"
+        f"* * * * *   ELECTION  RESULTS   * * * * *\n\n"
+        f"=========================================\n\n"
         f"Total votes: {total_votes:,}\n\n"
-        f"------------------\n\n"
+        f"-----------------------------------------\n"
+        f"Votes by County:\n"
+        f"-----------------------------------------\n\n"
         )
     print(election_results, end="")
     txt_file.write(election_results)
+    for county_name in county_votes:
+        cvotes = county_votes[county_name]
+        county_percentage = float(cvotes)/float(total_cvotes)*100
+        county_results = (f"{county_name}: {cvotes:,} ({county_percentage:.2f}%)\n")
+        print(county_results)
+        txt_file.write(county_results)
+        if (cvotes > turnout_count) and (county_percentage > turnout_percentage):
+            turnout_count = cvotes
+            turnout_percentage = county_percentage
+            turnout_highest = county_name
+    highest_turnout_summary = (
+        f"\n=========================================\n\n"
+        f"Highest voter turnout: {turnout_highest} County\n"
+        f"\n=========================================\n\n"
+        f"-----------------------------------------\n"
+        f" Votes by Candidate:\n"
+        f"-----------------------------------------\n\n"
+        )
+    print(highest_turnout_summary)
+    txt_file.write(highest_turnout_summary)
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes)/float(total_votes)*100
@@ -42,10 +80,11 @@ with open(file_to_save,"w") as txt_file:
             winning_percentage = vote_percentage
             winning_candidate = candidate_name
     winning_candidate_summary = (
-        f"\n==================\n\n"
-        f"Winner: {winning_candidate}\n"
-        f"Winning Vote Count: {winning_count:,} ({winning_percentage:.2f}%)\n\n"
-        f"==================\n"
+        f"\n=========================================\n\n"
+        f" Election Winner: {winning_candidate}\n\n"
+        f" Winning Vote Count: {winning_count:,} ({winning_percentage:.2f}%)\n\n"
+        f"=========================================\n\n"
+        f"* * * * * * * * * * * * * * * * * * * * *\n"
         )
     print(winning_candidate_summary)
     txt_file.write(winning_candidate_summary)
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # 03-Election_Analysis
  
+## Overview
+The Colorado Board of Elections requested we complete an audit of a recent local congressional election to determine the following information: 
+
+1. Total number of votes cast. 
+2. All candidates who received votes. 
+3. Total number of votes each candidate received. 
+4. Percentage of votes each candidate received. 
+5. Winner based on popular vote. 
+
+## Resources
+- Data Source: election_results.csv
+- Software: Python 3.9.0, Visual Studio Code 1.67.0
+
+## Summary 
+The analysis of the election shows: 
+- There were 369,711 votes cast
+- The candidates were: 
+    - Charles Casper Stockham
+    - Diana DeGette
+    - Raymon Anthony Doane
+- The vote distribution was: 
+    - Charles Casper Stockham: 85,213 (23.05%)
+    - Diana DeGette: 272,892 (73.81%)
+    - Raymon Anthony Doane: 11,606 (3.14%)
+- **Diana DeGette** won the elevtion with **272,892** (73.81%) **votes**
+
+## Challenge Overview
+The Colorado Board of Elections requested further analysis of the data: 
+
+1. Voter turnout for each county.
+2. Percentage of votes from each county.
+3. County with the highest turnout.
+
+## Challenge Summary
+The analysis of votes per county shows: 
+- The counties involved in the election were: 
+	- Jefferson
+	- Denver
+	- Arapahoe
+- Voter turnout per county was: 
+        - Jefferson: 38,855 (10.51%)
+        - Denver: 306,055 (82.78%)
+        - Arapahoe: 24,801 (6.71%)
+- **Denver County** had more than 4 times the voter turnout than Jefferson and Arapahoe counties combined. 
